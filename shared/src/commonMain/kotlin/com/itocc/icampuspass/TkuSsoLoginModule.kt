tkuitocc/icampuspass-kmp

Add TkuSsoLoginModule
Change-Id: I6987165502ccc8127e9500ac8a58558802f18a6c

shared/src/commonMain/kotlin/com/itocc/icampuspass/TkuSsoLoginModule.kt
@@ -1,0 +1,7 @@
+package com.itocc.icampuspass
+
+class TkuSsoLoginModule {
+    fun Login_varify(email: String, password: String) {
+
+    }
+}

winstonsung/icampuspass_kmp

shared/src/commonMain/kotlin/com/itocc/icampuspass/TkuSsoLoginModule.kt
@@ -1,0 +1,7 @@
+package com.itocc.icampuspass
+
+class TkuSsoLoginModule {
+    fun Login_verify(email: String, password: String) {
+
+    }
+}